446088fa29404823d76a7c769ed744d3d65adb77
wzy <twoeo@qq.com>
2017-08-25 20:13:12 +0800

    init

---
+++ index.d.ts
@@ -0,0 +1,4 @@
+/// <reference types="k8w-linq-array" />
+/// <reference types="k8w-super-date" />
+/// <reference types="k8w-super-object" />
+
---
+++ index.ts
@@ -0,0 +1,3 @@
+import 'k8w-linq-array';
+import 'k8w-super-date';
+import 'k8w-super-object';
