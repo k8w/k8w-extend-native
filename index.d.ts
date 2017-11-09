886283cdf42b07507224e0502713515d976475f4
wzy <twoeo@qq.com>
2017-11-09 14:49:08 +0800

    修复global BUG

--- index.d.ts
+++ index.d.ts
@@ -2,6 +2,8 @@ import 'k8w-linq-array';
 import 'k8w-super-date';
 import 'k8w-super-object';
 
-declare type Diff<T extends string, U extends string> = ({[P in T]: P } & {[P in U]: never } & { [x: string]: never })[T];
-declare type Omit<T, K extends keyof T> = {[P in Diff<keyof T, K>]: T[P]};
-declare type Overwrite<T, U> = {[P in Diff<keyof T, keyof U>]: T[P]} & U;
+declare global {
+    type Diff<T extends string, U extends string> = ({[P in T]: P } & {[P in U]: never } & { [x: string]: never })[T];
+    type Omit<T, K extends keyof T> = {[P in Diff<keyof T, K>]: T[P]};
+    type Overwrite<T, U> = {[P in Diff<keyof T, keyof U>]: T[P]} & U;
+}
