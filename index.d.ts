b448913cd6fb00342668a795461fa86522bbd3b1
k8w <twoeo@qq.com>
2018-07-19 10:32:22 +0800

    修复高版本TS报错

--- index.d.ts
+++ index.d.ts
@@ -3,7 +3,7 @@ import 'k8w-super-date';
 import 'k8w-super-object';
 
 declare global {
-    type Diff<T extends string, U extends string> = ({[P in T]: P } & {[P in U]: never } & { [x: string]: never })[T];
-    type Omit<T, K extends keyof T> = {[P in Diff<keyof T, K>]: T[P]};
-    type Overwrite<T, U> = {[P in Diff<keyof T, keyof U>]: T[P]} & U;
+    type Diff<T extends (string | number | symbol), U extends (string | number | symbol)> = ({ [P in T]: P } & { [P in U]: never } & { [x: string]: never })[T];
+    type Omit<T, K extends keyof T> = { [P in Diff<keyof T, K>]: T[P] };
+    type Overwrite<T, U> = { [P in Diff<keyof T, keyof U>]: T[P] } & U;
 }
