4c9282955f08f6695f9f0a519bc401023be3b585
k8w <me@k8w.io>
2019-07-22 15:58:09 +0800

    1.4.0

--- index.ts
+++ index.ts
@@ -7,7 +7,7 @@ export type Overwrite<T, U> = Pick<T, Exclude<keyof T, keyof U>> & U;
 //应对IE9以下没有console
 if (typeof window != 'undefined' && !window.console) {
     (window as any).console = {};
-    console.log = console.debug = console.info = console.warn = console.error = console.time = console.timeEnd = function(){}
+    console.log = console.debug = console.info = console.warn = console.error = console.time = console.timeEnd = function () { }
 }
 
 //应对某些浏览器没有console.debug的情况
