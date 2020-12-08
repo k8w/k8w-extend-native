2724d584afe18fa33133af3ba52316f65d3278a3
k8w <me@k8w.io>
2020-12-08 16:57:53 +0800

    1.4.2 Union Mapped Type Helper

--- index.ts
+++ index.ts
@@ -3,6 +3,9 @@ import 'k8w-super-date';
 import 'k8w-super-object';
 
 export type Overwrite<T, U> = T extends unknown ? Pick<T, Exclude<keyof T, keyof U>> & U : never;
+export type PickUnion<T, U extends keyof T> = T extends unknown ? Pick<T, U> : never;
+export type OmitUnion<T, U extends keyof T> = T extends unknown ? Omit<T, U> : never;
+export type PartialUnion<T> = T extends unknown ? Partial<T> : never;
 
 //应对IE9以下没有console
 if (typeof window != 'undefined' && !window.console) {
