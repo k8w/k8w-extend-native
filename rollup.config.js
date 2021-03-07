cac77f1a57f9d92611ac0961e5473a16b0e1b780
King Wang <me@k8w.io>
2021-03-07 21:27:43 +0800

    1.4.4 dual package

---
+++ rollup.config.js
@@ -0,0 +1,18 @@
+import typescript from 'rollup-plugin-typescript2';
+
+export default {
+    input: './index.ts',
+    output: [
+        {
+            format: 'cjs',
+            file: './dist/index.cjs',
+        },
+        {
+            format: 'es',
+            file: './dist/index.mjs',
+        }
+    ],
+    plugins: [
+        typescript()
+    ]
+}
